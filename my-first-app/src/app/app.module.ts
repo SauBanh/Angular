717350fe26e7edb1fa5c3aef6291092b16a96565
SauBanh <saubanh05062001@gmail.com>
2023-12-02 10:57:59 +0700

    what is angular

---
+++ my-first-app/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
+@NgModule({
+  declarations: [AppComponent, ServerComponent, ServersComponent, SuccessAlertComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ my-first-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: ` <q>This is a warning, you are in danger!</q> `,
+})
+export class WarningAlertComponent {}
