65bcbf35b019f79e756ef48021c83bfe1c88edd3
SauBanh <saubanh05062001@gmail.com>
2023-12-03 23:45:49 +0700

    learn basic, components, databinding

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Angular
+
+lifecycle
+
+| ng...              | description                                                              |
+| ------------------ | ------------------------------------------------------------------------ |
+| ngOnchanges        | Called after a bound input property changes                              |
+| ngOnlnit           | Called once the component is initialized                                 |
+| ngDoCheck          | Called during every change detection run                                 |
+| ngAfterContentlnit | Called after content (ng-content) has been projected into view           |
+| ngAfterViewlnit    | Called after the component's view (and child views) has been initialized |
+| ngAfterViewChecked | Called every time the view (and child views) have been checked           |
+| ngOnDestroy        | Called once the component is about to be destroyed                       |
+
+![Alt text](image.png)
+
+create new project Angular
+
+```terminal
+ng new your-name-project --no-strict --routing false --standalone false
+```
---
+++ component-agular/src/app/server-element/server-element.component.ts
@@ -0,0 +1,69 @@
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  Input,
+  SimpleChanges,
+  ViewChild,
+  // ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrl: './server-element.component.css',
+  // encapsulation: ViewEncapsulation.None,
+  // encapsulation: ViewEncapsulation.ShadowDom,
+  // encapsulation: ViewEncapsulation.Emulated, //None, Native
+})
+export class ServerElementComponent {
+  @Input('srvElement') element: { type: string; name: string; content: string };
+  @Input() name: string;
+  @ViewChild('heading', { static: true }) header: ElementRef;
+  @ContentChild('contentParagraph', { static: true }) paragraph: ElementRef;
+
+  constructor() {
+    console.log('Constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+    console.log(
+      'Text Content of paragraph: ' + this.paragraph.nativeElement.textContent
+    );
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+    console.log(
+      'Text Content of paragraph: ' + this.paragraph.nativeElement.textContent
+    );
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called');
+  }
+}
